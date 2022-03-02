7bcb79e32123f8d5250e9bdf6881398fc44e924d
Andre-Oberg <andre.oberg@ha.ax>
2022-03-02 19:09:45 +0200

    Satt in funktion sum samt test cases
    Satt in funktion sum som tar in en sträng och går igenom den en bokstav i taget och kollar ifall det är en siffra, siffran sparras sedan i en ny sträng vars värde hämtas ut när ett icke tal förekommer. Detta leder till att programmet bör kunna hantera tal som 12 istället för att tolka det som 3.

--- src/main/java/ax/ha/it/calculator/StringCalculator.java
+++ src/main/java/ax/ha/it/calculator/StringCalculator.java
@@ -2,11 +2,41 @@ package ax.ha.it.calculator;
 
 public class StringCalculator {
     
-    static int add(String numbers) {
-        if (numbers.isEmpty()) {
+    public int add(String text) {
+        if (text.isEmpty()) {
             return 0;
         } else { 
-            return 1;
+            return sum(text);
         }
+    }
+    
+    // Funktion för att addera alla potentiella tal som finns i strängen
+    private int sum(String string){
+        
+        // Sträng som kommer innehålla talen som hittas i input strängen
+        String num = "0";
+        int sum = 0;
+        
+        for (int i=0; i < string.length(); i++) {
+
+            // Ifall talet skulle vara exempelvis 10, 11 tolv så behöver man lägga till
+            // de extra siffrorna i en seperat sträng, sedan när en icke siffra förekommer så
+            // plockar man ut talet
+            if (Character.isDigit(string.charAt(i))) {
+                num += string.charAt(i);
+            } else {
+                sum += Integer.parseInt(num);
+                
+                // "Nollar" strängen så att inget blir kvar av det som fanns tidigare
+                num = "0";
+            }
+            
+            // Behöver ett sätt att hämta ut det sista i strängen 
+            if (i == string.length()-1) {
+                sum += Integer.parseInt(num);
+            }
+        }
+       
+        return sum;
     }
 }
--- src/test/java/ax/ha/it/calculator/StringCalculatorTest.java
+++ src/test/java/ax/ha/it/calculator/StringCalculatorTest.java
@@ -1,20 +1,56 @@
 package ax.ha.it.calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class StringCalculatorTest {
     
+    //private StringCalculator calc;
+    
+   /* @BeforeEach
+    public void init() {
+        calc = new StringCalculator();
+    }*/
+    
     @Test
-    public void testAddWithEmptyString() {
-        assertEquals(0, StringCalculator.add(""));
+    public void testEmptyString() {
+        StringCalculator calc2 = new StringCalculator();
+        assertEquals(0, calc2.add(""));
     }
     
     @Test
     public void testAddWithNotEmptyString() {
-        assertEquals(1, StringCalculator.add(" "));
+        StringCalculator calc2 = new StringCalculator();
+        assertEquals(1, calc2.add("1"));
     }
+    
+    @Test
+    public void testWithOneNumber() {
+        StringCalculator calc2 = new StringCalculator();
+        assertEquals(22, calc2.add("22"));
+    }
+    
+    @Test
+    public void testWithTwoNumbers() {
+        StringCalculator calc2 = new StringCalculator();
+        assertEquals(4, calc2.add("2 2"));
+    }
+    
+    @Test
+    public void testWithMultipleNumbers() {
+        StringCalculator calc2 = new StringCalculator();
+        assertEquals(37, calc2.add("5 5.22,2a3aa"));
+    }
+    
+        @Test
+    public void testWithNewLine() {
+        StringCalculator calc2 = new StringCalculator();
+        assertEquals(5, calc2.add("//1:;2\n2"));
+    }
+    
     
 }
